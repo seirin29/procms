2fc5182cd12132c6efd795d15052b401702f630d
seirin29 <31317917+seirin29@users.noreply.github.com>
2017-10-27 01:16:38 +0700

    Laravel Project Procms

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.admin')
+
+@section('content')
+	<h1>Create User</h1>
+	{!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+	
+	<div class="form-group">
+	{!! Form::label('name', 'Name :') !!}
+	{!! Form::text('name', null, ['class'=>'form-control']) !!}
+	</div>
+	
+	<div class="form-group">
+	{!! Form::label('email', 'Email :') !!}
+	{!! Form::email('email', null, ['class'=>'form-control']) !!}
+	</div>
+	
+	<div class="form-group">
+	{!! Form::label('role_id', 'Role :') !!}
+	{!! Form::text('role_id', null, ['class'=>'form-control']) !!}
+	</div>
+	
+	<div class="form-group">
+	{!! Form::label('is_active', 'Status :') !!}
+	{!! Form::text('is_active', null, ['class'=>'form-control']) !!}
+	</div>
+	
+	<div class="form-group">
+	{!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+	</div>
+	
+	{!! Form::close() !!}
+@stop
