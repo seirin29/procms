2279693a60c596f37b47b63e85bc73cdaa1ee0f4
seirin29 <31317917+seirin29@users.noreply.github.com>
2017-11-05 23:11:45 +0700

    Edit User

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -21,8 +21,8 @@
 			
 			<tr>
 				<td>{{$user->id}}</td>
-				<td><img height="50" src="/images/{{$user->photo ? $user->photo->file: 'no user photo'}}"</td>
-				<td>{{$user->name}}</td>
+				<td><img height="50" src="/images/{{$user->photo ? $user->photo->file: 'no user photo'}}" /></td>
+				<td><a href="{{route('admin.users.edit',$user->id)}}">{{$user->name}}</a></td>
 				<td>{{$user->email}}</td>
 				<td>{{$user->role->name}}</td>
 				<td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
